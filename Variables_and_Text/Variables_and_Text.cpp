eb159bc31925ac0677b3dcc809fe1a76a0598353
XVND83 <XVND83@XVND83-Z14G8>
2022-05-24 21:41:34 +0200

    Finished excercise Level 0

--- Variables_and_Text/Variables_and_Text.cpp
+++ Variables_and_Text/Variables_and_Text.cpp
@@ -14,41 +14,50 @@ int main()
 {
     // Setting initial values to variables
     robot_name = "Henio";
-    robot_status = "Idle"; // Exploring/Sampling/Returning/Idle
+    robot_status = "Exploring"; // type of status: Exploring/Sampling/Returning/Idle
+
+    // welcome sign
+    cout << "Hi! You are connected to program for checking your Mars rover." << endl;
  
     
     do {        
         //Pseudo-random integral value between ​0​ and RAND_MAX restricted to 100 by modulo operation
         battery_charge_status = rand() % 101;
+
         //https://stackoverflow.com/questions/686353/random-float-number-generation 
         temperatue = -125 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (200 + 125)));
         velocity = 0 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (20 - 0)));
-
-        cout << "Which number of variable to check?\n1. Robot's name\n2. Battery charge status\n3. Temperature and velocity\n4. Robot status" << endl;
-        cin >> user_input;
+        
+        cout << "\nWhich number of variable to check?\n1. Robot's name\n2. Battery charge status\n3. Temperature and velocity\n4. Robot status\n5. EXIT\n" << endl;
+        cout << "I want check: ", cin >> user_input;
         
         if (user_input == "1") {
-            cout << "Robot's name is: " << robot_name << endl;
-            cout << "Change my name to: " << endl;
+            cout << "\nRobot's name is: " << robot_name << endl;
+            cout << "Change my name to: "; 
             cin >> robot_name;
+            cout << "\nName changed to -------- " << robot_name << " --------" << endl;
+            cout << endl;
         }
         else if (user_input == "2") {
-            cout << "Battery charge status is: " << battery_charge_status << "%" << endl;
+            cout << "\nBattery charge status is: \n" << battery_charge_status << "%" << endl;
         }
         else if (user_input == "3") {
-            cout << "Temperature is: " << temperatue << char(248) <<"C.\nVelocity of the rover is: " << velocity << "km/h" << endl;
+            cout << "\nTemperature is: " << temperatue << char(248) <<"C.\nVelocity of the rover is: " << velocity << "km/h" << endl;
         }
         else if (user_input == "4") {
-            cout << "Robot status: " << robot_status << endl;
-            cout << "Change my status to: " << endl;
-            cin >> robot_status;
+            cout << "\nRobot status: " << robot_status << endl;
+            cout << "\nChange my status to [Exploring/Sampling/Returning/Idle]: ", cin >> robot_status;
+            cout << "\nStatus changed to -------- " << robot_status << " --------" << endl;
         }
+        else if (user_input == "5") {
+            cout << "See you next time! Bye!" << endl;
+            break;
+        }
+      
         else {
-            cout << "\n Wrong choice! Once again." << endl;
+            cout << "\n Wrong choice! Once again.\n" << endl;
         }
 
-    } while(true);
-
-
+    } while (true);
 
 }
